6fad009777c2fb1a719b2aec976d8161b68859b7
Esi Imbeah <eaitfr@umsystem.edu>
2023-10-19 23:21:04 -0500

    added dotenv dependency and fixed connectTimeout

--- web-server/config.js
+++ web-server/config.js
@@ -9,7 +9,7 @@ const config = {
         user                : process.env.DB_USERNAME,
         password            : process.env.DB_PASSWORD,
         database            : process.env.DB_NAME,
-        connectionTimeout   : 60000
+        connectTimeout      : 60000
     },
 }
 
