94eb62fde20a5219fbd669db1f2114d2e970c184
NowoTwo <cobjaysierko@gmail.com>
2023-10-20 14:25:12 -0500

    updated README and finished user service function

--- web-server/services/user.js
+++ web-server/services/user.js
@@ -1,11 +1,10 @@
 const db = require('.db');
-const hashing = require('.hashing');
-const helper = require('../helper');
+const mycrypt = require('.crypto');
 const config = require('../config');
 
 async function createAccount(user) {
     // Salt and hash the users password
-    const pass_hash = await hashing.generateHash(user.password);
+    const pass_hash = await mycrypt.generateHash(user.password);
     // Insert the new user into the database (with the hashed password)
     const result = await db.query(
         `INSERT INTO APP_USER (name, password, professor)
@@ -22,7 +21,7 @@ async function createAccount(user) {
 
 async function login(user) {
     // See if the user with the username is in the database
-    const get_user = await db.query(
+    var get_user = await db.query(
         `SELECT *
         FROM APP_USER
         WHERE NAME = '${user.name}'`
@@ -30,5 +29,19 @@ async function login(user) {
     if (!get_user) {
         get_user = [];
     }
+    else {
+        get_user = get_user[0];
+    }
+    // Compare the given password with the hash stored in the DB
+    var result = await mycrypt.compareHash(user.password, get_user.password);
+    if (!result) {
+        return null;
+    }
+    // Generate and return an access token
+    return mycrypt.generateToken(user.username);
+}
 
+module.exports = {
+    createAccount,
+    login
 }
