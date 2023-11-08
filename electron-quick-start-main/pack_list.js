8698766fc1f92a73d80b9ea189185270210174d2
NowoTwo <cobjaysierko@gmail.com>
2023-11-08 02:30:24 -0600

    buttons now display score info, as well as setting the current pack id in local storage

--- electron-quick-start-main/pack_list.js
+++ electron-quick-start-main/pack_list.js
@@ -1,4 +1,4 @@
-function addPack(name) {
+function addPack(id, name, points_total, pack_score) {
     // This will be the container that will contain the new pack
     const packContainer = document.getElementById("pack_container");
 
@@ -9,12 +9,23 @@ function addPack(name) {
     newButton.className = "pack";
     const packName = document.createTextNode(name)
     newButton.appendChild(packName)
+    // This is the buttons functionality
+    newButton.addEventListener('click', ()=> {
+        localStorage.setItem("currentPackID", id)
+        // put the navigation controls here
+    });
+
     // This is the base div for the progress bar
     const newProgressBar = document.createElement("div");
     newProgressBar.className = "pack-progress-bar";
     // This is the div that represents the user's score for this pack
     const newProgress = document.createElement("div");
     newProgress.className = "progress";
+    if (pack_score == null) { 
+        pack_score = 0; 
+        newProgressBar.style.backgroundColor = "#555"
+    }
+    newProgress.style.width = `${(pack_score/points_total)*100}%`;
 
     // Now we append up the list
     newProgressBar.appendChild(newProgress)
@@ -27,7 +38,7 @@ document.addEventListener('DOMContentLoaded', async function(event) {
     const mc_modal = document.getElementById('mc_modal');
 
     // We load in all the pack data here
-    await fetch('http://localhost:3000/pack', {
+    await fetch('http://localhost:3000/pack/user', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -45,7 +56,7 @@ document.addEventListener('DOMContentLoaded', async function(event) {
             for (let i = 0; i < data.data.length; i++) {
                 let obj = data.data[i];
         
-                addPack(obj.name);
+                addPack(obj.ID, obj.name, obj.points_total, obj.pack_score);
             }
         });
     
