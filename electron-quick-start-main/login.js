542e28400da3634e7366ce5307ff4617f768d44a
Gorman <gmanpate@gmail.com>
2023-11-09 17:37:09 -0600

    updated buttons

--- electron-quick-start-main/login.js
+++ electron-quick-start-main/login.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('login-form');
 
+    document.getElementById('back').addEventListener('click', () => {
+        window.myAPI.send('navigate', 'main');
+    });
+
     form.addEventListener('submit', async function(e) {
         e.preventDefault();
-
+        login_btn.textContent = 'Logging In...';
+        login_btn.disabled = true;
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
@@ -30,20 +35,26 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Use IPC to tell the main process to navigate to the main menu
                 window.myAPI.send('navigate', 'main_menu');
             } else {
+                
                 // Show error message using dialog API from the main process
                 await window.myAPI.invoke('show-message-box', {
                     type: 'error',
                     title: 'Login Error',
                     message: 'Error logging in. Please check your credentials.'
                 });
+                login_btn.textContent = "Login";
+                login_btn.disabled = false;
             }
         } catch (err) {
             console.error('Error:', err);
             // Show error message using dialog API from the main process
+            login_btn.textContent = "Login";
+            login_btn.disabled = false;
             await window.myAPI.invoke('show-message-box', {
                 type: 'error',
                 title: 'Login Error',
                 message: 'An error occurred during login. Please try again later.'
+                
             });
         }
     });
